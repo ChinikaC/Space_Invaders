2917d3817e12a89f4592cf91f45b398b8253b614
Chinika Charles <clcharles@hotmail.co.uk>
2023-08-17 00:13:26 +0100

    Added in code to make the player rotate to the right

--- index.js
+++ index.js
@@ -86,12 +86,14 @@ function animate() {
 
     if (keys.ArrowLeft.pressed && player.position.x >= 0){
         player.velocity.x = -5
-        player.rotation = -.15
+        player.rotation = -0.15
     } else if (keys.ArrowRight.pressed && 
         player.position.x + player.width <= canvas.width){
         player.velocity.x = 5
+        player.rotation = 0.15
     } else {
         player.velocity.x = 0
+        player.rotation = 0
     }
 }
 
