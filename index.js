67d617fb11cf34afaa0ff7191a68fe74b04c620d
Chinika Charles <clcharles@hotmail.co.uk>
2023-10-16 23:38:35 +0100

    Added in 'invader.width' and now the correct invadersare disappearing

--- index.js
+++ index.js
@@ -225,7 +225,7 @@ function animate() {
                     invader.position.y + invader.height &&
                     projectile.position.x + projectile.radius >= 
                     invader.position.x && projectile.position.x -
-                    projectile.radius <= invader.position.x &&
+                    projectile.radius <= invader.position.x + invader.width &&
                     projectile.position.y + projectile.radius >= 
                     invader.position.y){
                         setTimeout(() => {
