6b9b980bc5f0494b62eb144a05f677be99f26e52
Chinika Charles <clcharles@hotmail.co.uk>
2024-06-06 23:12:35 +0100

    Added in code so the particles fade out

--- index.js
+++ index.js
@@ -94,9 +94,12 @@ class Particle {
         this.velocity = velocity
         this.radius = radius
         this.color = color
+        this.opacity = 1
     }
 
     draw() {
+        context.save()
+        context.globalAlpha = this.opacity //globalAlpha sets the transparency of the context
         context.beginPath()
         context.arc(this.position.x,
             this.position.y,
@@ -106,12 +109,14 @@ class Particle {
         context.fillStyle = this.color
         context.fill()
         context.closePath()
+        context.restore()
     }
 
     update() {
         this.draw()
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
+        this.opacity -= 0.01
     }
 }
 
